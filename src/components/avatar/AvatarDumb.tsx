12b350361d9f30b91f4e4f310967a950382bf480
William Spada <william@pop-os.localdomain>
2023-12-30 12:09:53 -0300

    feat: close and open avatar menu

--- src/components/avatar/AvatarDumb.tsx
+++ src/components/avatar/AvatarDumb.tsx
@@ -13,11 +13,33 @@ import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import ReportProblemOutlinedIcon from '@mui/icons-material/ReportProblemOutlined';
+import { AvatarDumbProps } from './interfaces/AvatarDumb';
+import { useCallback, useEffect, useRef } from 'react';
 
-export function AvatarDumb() {
+export function AvatarDumb(props: AvatarDumbProps) {
+ const menuRef = useRef<HTMLUListElement | null>(null);
+
+ const openMenu = useCallback(() => {
+  props.handleMenu.setIsMenuOpen(true);
+ }, [props.handleMenu]);
+
+ const closeMenu = useCallback((e: Event) => {
+  if (menuRef.current && !menuRef.current?.contains(e.target as Node)) {
+   props.handleMenu.setIsMenuOpen(false);
+  }
+ }, [props.handleMenu]);
+
+ useEffect(() => {
+  document.addEventListener('click', closeMenu, true);
+
+  return () => {
+   document.removeEventListener('click', closeMenu, true);
+  }
+ }, [closeMenu]);
+
  return (
   <section className="flex relative gap-x-3">
-   <article className=''>
+   <article>
     <h3 className='text-base'>Cody Fisher</h3>
     <p className='text-sm text-[#919191]'>Last connected 8 minutes ago</p>
    </article>
@@ -25,68 +47,70 @@ export function AvatarDumb() {
     <li className="[clip-path:circle()] w-5 h-5 bg-[#FF4A4A] flex items-center justify-center">
      <span className="text-center text-white text-xs">3</span>
     </li>
-    <li className='cursor-pointer hover:opacity-50 hover:transition-all'>
+    <li className='cursor-pointer hover:opacity-50 hover:transition-all' onClick={openMenu}>
      <MoreVertIcon />
     </li>
    </ul>
-   <MenuList className='bg-[#737373] text-white !absolute !left-full'>
-    <MenuItem>
-     <ListItemIcon>
-      <CheckIcon fontSize="small" style={{ color: 'white' }} />
-     </ListItemIcon>
-     <ListItemText>Mark as Read</ListItemText>
-    </MenuItem>
-    <MenuItem>
-     <ListItemIcon>
-      <NotificationsOffIcon fontSize="small" style={{ color: 'white' }} />
-     </ListItemIcon>
-     <ListItemText>Turn off Notifications</ListItemText>
-    </MenuItem>
-    <MenuItem>
-     <ListItemIcon>
-      <PersonIcon fontSize="small" style={{ color: 'white' }} />
-     </ListItemIcon>
-     <ListItemText>Profile</ListItemText>
-    </MenuItem>
-    <Divider />
-    <MenuItem>
-     <ListItemIcon>
-      <CallIcon fontSize="small" style={{ color: 'white' }} />
-     </ListItemIcon>
-     <ListItemText>Call</ListItemText>
-    </MenuItem>
-    <MenuItem>
-     <ListItemIcon>
-      <VideocamIcon fontSize="small" style={{ color: 'white' }} />
-     </ListItemIcon>
-     <ListItemText>Video Call</ListItemText>
-    </MenuItem>
-    <Divider />
-    <MenuItem>
-     <ListItemIcon>
-      <HelpOutlineIcon fontSize="small" style={{ color: 'white' }} />
-     </ListItemIcon>
-     <ListItemText>Block</ListItemText>
-    </MenuItem>
-    <MenuItem>
-     <ListItemIcon>
-      <Inventory2OutlinedIcon fontSize="small" style={{ color: 'white' }} />
-     </ListItemIcon>
-     <ListItemText>Move to Box</ListItemText>
-    </MenuItem>
-    <MenuItem>
-     <ListItemIcon>
-      <DeleteOutlineIcon fontSize="small" style={{ color: 'white' }} />
-     </ListItemIcon>
-     <ListItemText>Delete Conversation</ListItemText>
-    </MenuItem>
-    <MenuItem>
-     <ListItemIcon>
-      <ReportProblemOutlinedIcon fontSize="small" style={{ color: 'white' }} />
-     </ListItemIcon>
-     <ListItemText>Report</ListItemText>
-    </MenuItem>
-   </MenuList>
+   {props.handleMenu.isMenuOpen && (
+    <MenuList className='bg-[#413e3e] text-white !absolute !left-full' ref={menuRef}>
+     <MenuItem>
+      <ListItemIcon>
+       <CheckIcon fontSize="small" style={{ color: 'white' }} />
+      </ListItemIcon>
+      <ListItemText>Mark as Read</ListItemText>
+     </MenuItem>
+     <MenuItem>
+      <ListItemIcon>
+       <NotificationsOffIcon fontSize="small" style={{ color: 'white' }} />
+      </ListItemIcon>
+      <ListItemText>Turn off Notifications</ListItemText>
+     </MenuItem>
+     <MenuItem>
+      <ListItemIcon>
+       <PersonIcon fontSize="small" style={{ color: 'white' }} />
+      </ListItemIcon>
+      <ListItemText>Profile</ListItemText>
+     </MenuItem>
+     <Divider />
+     <MenuItem>
+      <ListItemIcon>
+       <CallIcon fontSize="small" style={{ color: 'white' }} />
+      </ListItemIcon>
+      <ListItemText>Call</ListItemText>
+     </MenuItem>
+     <MenuItem>
+      <ListItemIcon>
+       <VideocamIcon fontSize="small" style={{ color: 'white' }} />
+      </ListItemIcon>
+      <ListItemText>Video Call</ListItemText>
+     </MenuItem>
+     <Divider />
+     <MenuItem>
+      <ListItemIcon>
+       <HelpOutlineIcon fontSize="small" style={{ color: 'white' }} />
+      </ListItemIcon>
+      <ListItemText>Block</ListItemText>
+     </MenuItem>
+     <MenuItem>
+      <ListItemIcon>
+       <Inventory2OutlinedIcon fontSize="small" style={{ color: 'white' }} />
+      </ListItemIcon>
+      <ListItemText>Move to Box</ListItemText>
+     </MenuItem>
+     <MenuItem>
+      <ListItemIcon>
+       <DeleteOutlineIcon fontSize="small" style={{ color: 'white' }} />
+      </ListItemIcon>
+      <ListItemText>Delete Conversation</ListItemText>
+     </MenuItem>
+     <MenuItem>
+      <ListItemIcon>
+       <ReportProblemOutlinedIcon fontSize="small" style={{ color: 'white' }} />
+      </ListItemIcon>
+      <ListItemText>Report</ListItemText>
+     </MenuItem>
+    </MenuList>
+   )}
   </section>
  );
 }
--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -1,13 +1,14 @@
 import AvatarLib from '@mui/material/Avatar';
 import { AvatarDumb } from './AvatarDumb';
+import { useState } from 'react';
 
 export function Avatar() {
- // const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+ const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
  return (
   <figure className='flex gap-x-2'>
    <AvatarLib className='after:absolute !overflow-visible after:bottom-0 after:right-1 relative after:content-normal after:w-2 after:h-2 after:[clip-path:circle()] after:bg-green-400'>WS</AvatarLib>
    <figcaption>
-    <AvatarDumb />
+    <AvatarDumb handleMenu={{ isMenuOpen, setIsMenuOpen }} />
    </figcaption>
   </figure>
  );
