9bcc4452c697ef521af714258984e73dd6c8bf2b
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2024-02-11 17:55:05 -0300

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/Home";
 
 function App() {
-  return <Home />;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App
