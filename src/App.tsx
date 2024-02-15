3f9cdf3486a9f9ee4567a80a01644c1c18d54ecc
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2024-02-15 18:59:25 -0300

    chore: add required props chat panel

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     },
     {
       path: '/chat/:chatId',
-      element: <ChatPanel />,
+      element: <ChatPanel chat={[]} onClickAboutContact={() => {}} />,
     },
     {
       path: '/about-contact/:userId',
