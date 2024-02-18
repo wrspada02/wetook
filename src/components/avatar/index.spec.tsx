8a2a4c196aae88b6ef6185eecbe929c79bea3539
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2024-02-18 17:46:04 -0300

    feat: start avatar props unit tests

--- src/components/avatar/index.spec.tsx
+++ src/components/avatar/index.spec.tsx
@@ -1,5 +1,26 @@
-import { expect, it } from 'vitest';
+import { cleanup, render, screen } from '@testing-library/react';
+import {
+  afterEach,
+  describe, expect, it,
+} from 'vitest';
+import { Avatar } from '.';
 
-it('should sum the numbers', () => {
-  expect(1 + 1).toBe(2);
+describe('Props avatar test', () => {
+  afterEach(cleanup);
+
+  it('should render group component if passed props as true', () => {
+    render(<Avatar isGroup isHeaderAvatar isColumnStyle />);
+
+    const groupAvatarElement = screen.getByTestId('group-avatar');
+
+    expect(groupAvatarElement).toBeDefined();
+  });
+
+  it('should not render group component if passed props as false', () => {
+    render(<Avatar isGroup={false} isHeaderAvatar isColumnStyle />);
+
+    const groupAvatarElement = screen.queryByTestId('group-avatar');
+
+    expect(groupAvatarElement).toBeNull();
+  });
 });
